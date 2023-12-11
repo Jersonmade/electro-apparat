a354045ee6139e5587a5cdf362c7c507342da769
Jersonmade <dima.sergeev.0404@inbox.ru>
2023-12-11 19:14:48 +0400

    first commit

---
+++ src/main/java/com/eaprovide/electroapparat/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.eaprovide.electroapparat.controller;
+
+import com.eaprovide.electroapparat.dto.product.ProductRequest;
+import com.eaprovide.electroapparat.dto.product.ProductResponse;
+import com.eaprovide.electroapparat.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/eaprovide/electroapparat/dto/MailOrderStructure.java
@@ -0,0 +1,14 @@
+package com.eaprovide.electroapparat.dto;
+
+import lombok.Data;
+
+@Data
+public class MailOrderStructure {
+    private String surname;
+    private String name;
+    private String nameOfOrganization;
+    private String inn;
+    private String email;
+    private String numberOfTelephone;
+    private String address;
+}
