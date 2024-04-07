3a0ad2e880ab274bc890908f76fe44c2141db996
Jersonmade <dima.sergeev.0404@inbox.ru>
2024-04-07 16:15:53 +0400

    changed mailDto

--- src/main/java/com/eaprovide/electroapparat/dto/mail/MailOrderStructure.java
+++ src/main/java/com/eaprovide/electroapparat/dto/mail/MailOrderStructure.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class MailOrderStructure {
+    private String Product;
+    private String Price;
     private String surname;
     private String name;
     private String nameOfOrganization;
